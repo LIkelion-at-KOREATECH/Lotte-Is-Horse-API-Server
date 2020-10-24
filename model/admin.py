4015e1ea0fee8d5086eb048135ff3497be6c219d
김승규 <airaen0129@gmail.com>
2020-10-25 04:02:36 +0900

    gitignore 적용

--- model/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Store, Product, Sell
-
-# Register your models here.
-admin.site.register(Store)
-admin.site.register(Product)
-admin.site.register(Sell)
