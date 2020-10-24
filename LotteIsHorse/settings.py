a209785f3f6feb67606d7dd390eb209a2b5e00fe
김승규 <airaen0129@gmail.com>
2020-10-25 05:02:00 +0900

    deployV9
    django==2.1.1

--- LotteIsHorse/settings.py
+++ LotteIsHorse/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
-from pathlib import Path
+# from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -32,7 +33,7 @@ ALLOWED_HOSTS = ['django-env.eba-cvhk3pnq.ap-northeast-2.elasticbeanstalk.com']
 INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
-    # 'model.apps.ModelConfig',
+    'model.apps.ModelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'LotteIsHorse.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), #BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -119,20 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images, Fonts)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'LotteIsHorse' / 'static',
-    BASE_DIR / 'appMain' / 'static',
-    BASE_DIR / 'appUser' / 'static',
-]
-
-STATIC_ROOT = BASE_DIR / 'static'
-
 # Media files
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media'), # BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
